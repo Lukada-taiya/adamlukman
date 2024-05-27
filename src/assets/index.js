867f375c1d0b318bfcc28c84c90f2e7ef0d21eed
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-27 10:02:37 +0000

    Initial Commit

---
+++ src/assets/index.js
@@ -0,0 +1,97 @@
+import bwmap from './backgrounds/bw-map.jpeg';
+import nairobi from './backgrounds/nairobi.png';
+import whiteabstract from './backgrounds/white-abstract.png';
+import worldmap from './backgrounds/world-map.png';
+ 
+import logotext from './logo/logo-text-gold.png';
+import logo from './logo/logo-adam.png';
+
+import adam from './personal/adam.png';
+
+import backend from './icons/backend.png';
+import ux from './icons/ux.png';
+import frontend from './icons/frontend.png';
+import prototyping from './icons/prototyping.png';
+import github from './icons/github.png';
+import close from './icons/close.png';
+import download from './icons/download.png';
+import downloadHover from './icons/downloadHover.png';
+import pineapple from './icons/pineapple.png';
+import pineappleHover from './icons/pineappleHover.png';
+import menu from './icons/menu.png';
+import resume from './icons/resume.png';
+import send from './icons/send.png';
+import sendHover from './icons/sendHover.png';
+
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import figma from './tech/figma.png'; 
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import typescript from './tech/typescript.png';
+import graphql from './tech/graphql.png';
+import postgresql from './tech/postgresql.png'; 
+import laravel from './tech/laravel.png';
+import mongo from './tech/mongo.png';
+import csharp from './tech/c-sharp.png'
+import dotnet from './tech/dotnet.png'
+import vue from './tech/vue.png';
+ 
+import innorik from './company/innorik.png';
+import freelance from './company/freelance.png';
+
+import faculty from './projects/faculty_mis.png';
+import inventory from './projects/inventory.png';
+import banana from './projects/banana.png'; 
+import eventsmgr from './projects/eventsmgr.png';  
+
+export {
+  bwmap,
+  nairobi,
+  whiteabstract,
+  worldmap,
+  logo,
+  logotext,
+  adam,
+  backend,
+  ux,
+  frontend,
+  prototyping,
+  github,
+  close,
+  download,
+  downloadHover,
+  pineapple,
+  pineappleHover,
+  menu,
+  resume,
+  send,
+  sendHover,
+  css,
+  docker,
+  dotnet,
+  figma, 
+  graphql,
+  html,
+  javascript,
+  vue,
+  nodejs,
+  freelance,
+  innorik,
+  postgresql,
+  mongo,
+  csharp, 
+  laravel,
+  reactjs,
+  redux,
+  tailwind,
+  typescript, 
+  eventsmgr, 
+  banana,
+  inventory,
+  faculty,   
+};
---
+++ src/constants/index.js
@@ -0,0 +1,263 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs, 
+  tailwind,
+  nodejs, 
+  figma,
+  docker,
+  postgresql, 
+  mongo,
+  graphql, 
+  eventsmgr,  
+  dotnet,
+  vue, 
+  csharp, 
+  laravel,
+  freelance,
+  innorik,
+  faculty,
+  banana,
+  inventory
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'UI/UX Design',
+    icon: ux,
+  },
+  {
+    title: 'Software Prototyping',
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'Vue',
+    icon: vue,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'C-Sharp',
+    icon: csharp,
+  },
+  {
+    name: 'Dotnet',
+    icon: dotnet,
+  },
+  {
+    name: 'Laravel',
+    icon: laravel,
+  },
+  {
+    name: 'Mongo',
+    icon: mongo,
+  },
+  {
+    name: 'postgresql',
+    icon: postgresql,
+  }, 
+  {
+    name: 'graphql',
+    icon: graphql,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Full-Stack Developer',
+    company_name: 'Freelance',
+    icon: freelance,
+    iconBg: '#333333',
+    date: 'Jan 2019 - May 2023',
+  },
+  {
+    title: 'Full-Stack Developer',
+    company_name: 'Innorik Gh Ltd',
+    icon: innorik,
+    iconBg: '#333333',
+    date: 'Oct 2023 - May 2023',
+  }
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'FacultyMIS',
+    description: 'A faculty management system to facilitate effective communication between faculty members',
+    tags: [
+      {
+        name: 'php',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'laravel',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: faculty,
+    repo: 'https://github.com/Lukada-taiya/FacultyMIS',
+    demo: 'https://shaqdeff.github.io/KomiKult/',
+  },
+  {
+    id: 'project-2',
+    name: 'Events Manager',
+    description:
+      'An app to manage participants of scheduled events',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'node',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: eventsmgr,
+    repo: 'https://github.com/Lukada-taiya/events-mgr',
+    demo: 'https://github.com/Lukada-taiya/events-mgr',
+  },
+  {
+    id: 'project-3',
+    name: 'InventoryMS',
+    description: 'An app to store and manage organization inventory built with React',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: '.net',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: inventory,
+    repo: 'https://github.com/Lukada-taiya/InventoryMS',
+    demo: 'https://github.com/Lukada-taiya/InventoryMS',
+  },
+  {
+    id: 'project-4',
+    name: 'Banana Services',
+    description: `An Ecommerce Microservices .Net Application`,
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: banana,
+    repo: 'https://github.com/Lukada-taiya/BananaServices',
+    demo: 'https://github.com/Lukada-taiya/BananaServices',
+  },
+  // {
+  //   id: 'project-5',
+  //   name: 'Nyeusi Fest Site',
+  //   description:
+  //     'This is a demo concert website for a music festival called Nyeusi.',
+  //   tags: [
+  //     {
+  //       name: 'nextjs',
+  //       color: 'blue-text-gradient',
+  //     },
+  //     {
+  //       name: 'supabase',
+  //       color: 'green-text-gradient',
+  //     },
+  //     {
+  //       name: 'css',
+  //       color: 'pink-text-gradient',
+  //     },
+  //   ],
+  //   image: nyeusi,
+  //   repo: 'https://github.com/shaqdeff/Nyeusi-Fest-Site',
+  //   demo: 'https://shaqdeff.github.io/Nyeusi-Fest-Site/',
+  // },
+];
+
+export { services, technologies, experiences, projects };
